5ee7dda6f6cfb5878ceb35f8b7e1f0c9dc251736
__lleq <ball_090573@hotmail.com>
2024-10-22 10:27:54 +0700

    Part 5 : Functions and Higher-Order Functions

--- Part_5.ts
+++ Part_5.ts
@@ -8,6 +8,12 @@ function createBonusAdder(bonus : number)
     }
 }
 
+function bonusAdder(student : Student, bonus : number)
+{
+    const bonusGrade = createBonusAdder(bonus);
+    student.grades = student.grades.map(bonusGrade);
+}
+
 const m_Student1 = new Student("A", 21);
 m_Student1.addGrade(90);
 m_Student1.addGrade(75);
@@ -26,10 +32,9 @@ m_Student3.addGrade(55);
 m_Student3.addGrade(80);
 console.log(`${m_Student3.name} average grade = ${m_Student3.getAverageGrade()}`);
 
-const Bonus = createBonusAdder(5);
-m_Student1.grades = m_Student1.grades.map(Bonus);
-m_Student2.grades = m_Student2.grades.map(Bonus);
-m_Student3.grades = m_Student3.grades.map(Bonus);
+bonusAdder(m_Student1, 5);
+bonusAdder(m_Student2, 5);
+bonusAdder(m_Student3, 5);
 
 console.log(`${m_Student1.name} average grade = ${m_Student1.getAverageGrade()}`);
 console.log(`${m_Student2.name} average grade = ${m_Student2.getAverageGrade()}`);
