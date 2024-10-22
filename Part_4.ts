4e33314ead8df312ec7c5521e194c935cebb7f7c
__lleq <ball_090573@hotmail.com>
2024-10-22 12:05:42 +0700

    Update README

--- Part_4.ts
+++ Part_4.ts
@@ -7,11 +7,11 @@ const m_Student1 = new Student("A", 21);
 const m_Student2 = new Student("B", 19);
 m_dbStudent.addEntry(m_Student1);
 m_dbStudent.addEntry(m_Student2);
-console.log(`Student Entries in Databae: ${m_dbStudent.getAllEntries()}`);
+console.log("Student Entries in Databae: ", m_dbStudent.getAllEntries());
 
 const m_dbTeacher = new Database<Teacher>();
-const m_Teacher1 = new Teacher("Who?", 40, "Internet Programming");
-const m_Teacher2 = new Teacher("Who?", 40, "Cloud Computing");
+const m_Teacher1 = new Teacher("Bigboss", 40, "Internet Programming");
+const m_Teacher2 = new Teacher("Poppy", 34, "Cloud Computing");
 m_dbTeacher.addEntry(m_Teacher1);
 m_dbTeacher.addEntry(m_Teacher2);
-console.log(`Teacher Entries in Databae: ${m_dbTeacher.getAllEntries()}`);
+console.log("Teacher Entries in Databae: ", m_dbTeacher.getAllEntries());
