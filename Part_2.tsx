2a026c5e78a2badedbe2a601f74409f0eaa0dfc6
__lleq <ball_090573@hotmail.com>
2024-10-22 09:28:09 +0700

    Part 2 : Inheritance and Polymorphism

---
+++ Part_2.tsx
@@ -0,0 +1,3 @@
+const m_Teacher = new Teacher("Who?", 40, "Internet Programming");
+const m_Student = new Student("A", 21);
+m_Teacher.assignGrades(m_Student, 85);
---
+++ Teacher.tsx
@@ -0,0 +1,18 @@
+class Teacher extends Person
+{
+    subject : string;
+    students : Student[];
+
+    constructor(name : string, age : number, subject : string)
+    {
+        super(name, age);
+        this.subject = subject;
+        this.students = [];
+    }
+
+    assignGrades(student : Student, grade : number) : void
+    {
+        console.log(`(${this.subject}) ${this.name} assign grade to ${student.name} = ${grade}`);
+        student.addGrade(grade);
+    }
+}
